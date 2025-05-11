9429b0c206c583ef3bafe933d730165dd455e0ad
Victoria-290 <2419065@mail.ru>
2025-05-11 15:52:04 +0300

    управление работой функций с помощью context

--- Progect/internal/service/store_async.go
+++ Progect/internal/service/store_async.go
@@ -1,36 +1,29 @@
 package service
 
 import (
+	"context"
+
 	"github.com/Victoria-290/home-work-otus/Progect/internal/model/auth"
 	"github.com/Victoria-290/home-work-otus/Progect/internal/model/task"
 	"github.com/Victoria-290/home-work-otus/Progect/internal/model/user"
 	"github.com/Victoria-290/home-work-otus/Progect/internal/repository"
 )
 
-// EntityEvent — структура для передачи сущностей через канал
-type EntityEvent struct {
-	Type  string      // "user", "task", "token"
-	Value interface{} // указатель на соответствующую структуру
-}
-
-// StoreFromChannel получает события из канала и вызывает соответствующие методы хранения
-func StoreFromChannel(ch <-chan EntityEvent) {
-	for e := range ch {
-		switch e.Type {
-		case "user":
-			if u, ok := e.Value.(*user.User); ok {
-				repository.StoreUser(u)
+// StoreFromChannel — функция получения сущностей из канала и сохранения в соответствующий слайс, завершение по context
+func StoreFromChannel(ctx context.Context, ch <-chan EntityEvent) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case e := <-ch:
+			switch v := e.(type) {
+			case *user.User:
+				repository.StoreUser(v)
+			case *task.Task:
+				repository.StoreTask(v)
+			case *auth.Token:
+				repository.StoreToken(v)
 			}
-		case "task":
-			if t, ok := e.Value.(*task.Task); ok {
-				repository.StoreTask(t)
-			}
-		case "token":
-			if tok, ok := e.Value.(*auth.Token); ok {
-				repository.StoreToken(tok)
-			}
-		default:
-			// необработанный тип
 		}
 	}
 }
